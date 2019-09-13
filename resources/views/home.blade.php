d18fb006b32829cb7c07d9036fc9aaf44d726731
lemonuchujin <hoshina0713@yahoo.co.jp>
2019-09-13 20:20:20 +0900

    ログイン修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,17 +8,21 @@
                     <h4>全国の「おいしいもの」を投稿するサイトです<br><br>
                         みんなで「BiMMy!(美味ぃ!)」を共有しましょう
                     </h4>
-                    <div class='login'>
-                        <a class="btn btn-primary" href="{{ route('login') }}">{{ __('ログイン') }}</a>
-                        <a class="btn btn-success" href="{{ route('register') }}">{{ __('新規登録') }}</a>
-                    </div>
                     @if(!Auth::check())
+                    <div class='login'>
+                        <a class="btn btn-primary" href="{{ route('login') }}">
+                          {{ __('ログイン') }}</a>
+                        <a class="btn btn-success" href="{{ route('register') }}">
+                          {{ __('新規登録') }}</a>
+                    </div>
+                    <div class='test-login'>
                         <form action="{{ route('login') }}" method="POST" class="mt-3">
                         @csrf
                         <input type="hidden" name="email" value="test@test.jp">
                         <input type="hidden" name="password" value="testtesttest">
                         <button type="submit" class="btn btn-warning">テストユーザーでログイン</button>
                         </form>
+                    </div>
                     @endif
                 </div>
             </div>
